c9843b223c65d72a53a2336734acd313b70299c5
Sharan <poojarisharancr7@gmail.com>
2026-09-24 19:44:22 +0530

    feat: improve frontend security

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -62,9 +62,8 @@ export const useAuthStore = create((set, get) => ({
     if (!authUser || get().socket?.connected) return;
 
     const socket = io(BASE_URL, {
-      query: { userId: authUser._id },
+      withCredentials: true,
     });
-    socket.connect();
 
     set({ socket });
 
